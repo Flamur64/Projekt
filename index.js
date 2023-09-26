94bd5f11000f3fd14f76720e132ea02f0d673364
Flamur64 <Flamurim64@gmail.com>
2023-09-26 15:23:28 +0200

    Verbesserung

--- index.js
+++ index.js
@@ -5,30 +5,28 @@ const app = express()
 const port = 3000
 
 app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-app.use(bodyParser.json());
-let tasksData = [];
-
+let tasksData = []
 
 app.get("/", (req, res) => {
   res.send('<a href="/anmeldung">Zur Anmeldung</a>')
 })
 
 app.get("/anmeldung", function (req, res) {
-    res.sendFile("anmeldung.html", { root: "./" })
+  res.sendFile("anmeldung.html", { root: "./" })
 })
 
 app.get("/tasks", function (req, res) {
-  res.sendFile("tasks.html", { root: "./" });
-});
-
+  res.sendFile("tasks.html", { root: "./" })
+})
 
 app.post("/anmeldung", function (req, res) {
   const { name, password } = req.body
   if (name === "gruppe5" && password === "12345") {
     res.redirect("/tasks")
   } else {
-    res.send("Anmeldung fehlgeschlagen. Bitte überprüfen Sie Ihre Anmeldeinformationen.")
+    res.send("Anmeldung fehlgeschlagen.")
   }
 })
 
